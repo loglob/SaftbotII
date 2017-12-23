3c15dc546027a6e495055aa35a90914d0baacf50
Gruem <l.gruem@web.de>
2017-12-23 01:18:59 +0100

    Updated the helplink

--- SaftbotII/Exceptions/SaftEceptions.cs
+++ SaftbotII/Exceptions/SaftEceptions.cs
@@ -4,7 +4,7 @@ namespace SaftbotII.Exceptions
 {
     internal class SaftEceptions : Exception
     {
-        public const string repoLink = @"https://github.com/loglob/SaftbotNET";
+        public const string repoLink = @"https://github.com/loglob/SaftbotII";
 
         public SaftEceptions(string message = "", Exception innerException = null) : base(message, innerException)
         {
